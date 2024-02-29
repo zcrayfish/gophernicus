11c63d50635d82de963453dd1f97145c6aff7175
Zachary Lee Andrews <zachary@gopher.zcrayfish.soy>
2024-02-29 00:04:21 -0500

    Update test.sh: shellcheck improvements WIP

--- .travis/test.sh
+++ .travis/test.sh
@@ -1,5 +1,5 @@
-#!/bin/ash
-readonly TEMPDIR=$(mktemp -d /tmp/gophernicusXXXXXX) || exit
+#! /bin/sh
+readonly TEMPDIR="$(mktemp -d /tmp/gophernicusXXXXXX)" || exit
 
 if uname | grep -q 'Darwin' ; then
     # I do not have hardware to make this work on and am uninterested in
@@ -7,59 +7,59 @@ if uname | grep -q 'Darwin' ; then
     exit 0
 fi
 
-mkdir -p $TEMPDIR
-cp .travis/test.gophermap $TEMPDIR/gophermap
-chmod 755 $TEMPDIR
-chmod 644 $TEMPDIR/gophermap
+mkdir -p "$TEMPDIR"
+cp .travis/test.gophermap "$TEMPDIR/gophermap"
+chmod 755 "$TEMPDIR"
+chmod 644 "$TEMPDIR/gophermap"
 
 #test 1, test a basic gophermap,
 echo -e "\n" | src/gophernicus -h test.invalid -nf -nu -nv -nx -nf -nd -nr -r $TEMPDIR > $TEMPDIR/test1.output
-if ! cmp .travis/test1.answer $TEMPDIR/test1.output ; then
+if ! cmp .travis/test1.answer "$TEMPDIR/test1.output" ; then
     echo test1 round1 FAILED, test info saved at $TEMPDIR, diff between expected output and actual output:
-    diff .travis/test1.answer $TEMPDIR/test1.output
+    diff .travis/test1.answer "$TEMPDIR/test1.output"
     exit 1
 else
    echo test1 round1 passed
 fi
 echo -e "\r\n" | src/gophernicus -h test.invalid -nf -nu -nv -nx -nf -nd -nr -r $TEMPDIR > $TEMPDIR/test1.output
-if ! cmp .travis/test1.answer $TEMPDIR/test1.output ; then
+if ! cmp .travis/test1.answer "$TEMPDIR/test1.output" ; then
     echo test1 round2 FAILED, test info saved at $TEMPDIR, diff between expected output and actual output:
-    diff .travis/test1.answer $TEMPDIR/test1.output
+    diff .travis/test1.answer "$TEMPDIR/test1.output"
     exit 1
 else
    echo test1 round2 passed
 fi
 #test 1, test a basic gophermap
 echo -e "/\n" | src/gophernicus -h test.invalid -nf -nu -nv -nx -nf -nd -nr -r $TEMPDIR > $TEMPDIR/test1.output
-if ! cmp .travis/test1.answer $TEMPDIR/test1.output ; then
+if ! cmp .travis/test1.answer "$TEMPDIR/test1.output" ; then
     echo test1 round3 FAILED, test info saved at $TEMPDIR, diff between expected output and actual output:
-    diff .travis/test1.answer $TEMPDIR/test1.output
+    diff .travis/test1.answer "$TEMPDIR/test1.output"
     exit 1
 else
    echo test1 round3 passed
 fi
 echo -e "/\r\n" | src/gophernicus -h test.invalid -nf -nu -nv -nx -nf -nd -nr -r $TEMPDIR > $TEMPDIR/test1.output
-if ! cmp .travis/test1.answer $TEMPDIR/test1.output ; then
+if ! cmp .travis/test1.answer "$TEMPDIR/test1.output" ; then
     echo test1 round4 FAILED, test info saved at $TEMPDIR, diff between expected output and actual output:
-    diff .travis/test1.answer $TEMPDIR/test1.output
+    diff .travis/test1.answer "$TEMPDIR/test1.output"
     exit 1
 else
    echo test1 round4 passed
 fi
 #test 1, test a basic gophermap
 echo -e "GET /\n" | src/gophernicus -h test.invalid -nf -nu -nv -nx -nf -nd -nr -r $TEMPDIR > $TEMPDIR/test1.output
-if ! cmp .travis/test1.answer $TEMPDIR/test1.output ; then
+if ! cmp .travis/test1.answer "$TEMPDIR/test1.output" ; then
     echo test1 round5 FAILED, test info saved at $TEMPDIR, diff between expected output and actual output:
-    diff .travis/test1.answer $TEMPDIR/test1.output
+    diff .travis/test1.answer "$TEMPDIR/test1.output"
     exit 1
 else
    echo test1 round5 passed
 fi
 #test 1, test a basic gophermap
 echo -e "GET /\r\n" | src/gophernicus -h test.invalid -nf -nu -nv -nx -nf -nd -nr -r $TEMPDIR > $TEMPDIR/test1.output
-if ! cmp .travis/test1.answer $TEMPDIR/test1.output ; then
+if ! cmp .travis/test1.answer "$TEMPDIR/test1.output" ; then
     echo test1 round6 FAILED, test info saved at $TEMPDIR, diff between expected output and actual output:
-    diff .travis/test1.answer $TEMPDIR/test1.output
+    diff .travis/test1.answer "$TEMPDIR/test1.output"
     exit 1
 else
    echo test1 round6 passed
@@ -67,9 +67,9 @@ fi
 
 #test 2 test a non-existant file
 echo -e "FAKEFILE\n" | src/gophernicus -h test.invalid -nf -nu -nv -nx -nf -nd -nr -r $TEMPDIR > $TEMPDIR/test2.output
-if ! cmp .travis/test2.answer $TEMPDIR/test2.output ; then
+if ! cmp .travis/test2.answer "$TEMPDIR/test2.output" ; then
     echo test2 round1 FAILED, test info saved at $TEMPDIR, diff between expected output and actual output:
-    diff .travis/test2.answer $TEMPDIR/test2.output
+    diff .travis/test2.answer "$TEMPDIR/test2.output"
     exit 1
 else
    echo test2 round1 passed
@@ -77,9 +77,9 @@ fi
 
 #test 2 test a non-existant file
 echo -e "FAKEFILE\r\n" | src/gophernicus -h test.invalid -nf -nu -nv -nx -nf -nd -nr -r $TEMPDIR > $TEMPDIR/test2.output
-if ! cmp .travis/test2.answer $TEMPDIR/test2.output ; then
+if ! cmp .travis/test2.answer "$TEMPDIR/test2.output" ; then
     echo test2 round2 FAILED, test info saved at $TEMPDIR, diff between expected output and actual output:
-    diff .travis/test2.answer $TEMPDIR/test2.output
+    diff .travis/test2.answer "$TEMPDIR/test2.output"
     exit 1
 else
    echo test2 round2 passed
@@ -87,9 +87,9 @@ fi
 
 #test 2 test a non-existant file
 echo -e "/FAKEFILE\n" | src/gophernicus -h test.invalid -nf -nu -nv -nx -nf -nd -nr -r $TEMPDIR > $TEMPDIR/test2.output
-if ! cmp .travis/test2.answer $TEMPDIR/test2.output ; then
+if ! cmp .travis/test2.answer "$TEMPDIR/test2.output" ; then
     echo test2 round3 FAILED, test info saved at $TEMPDIR, diff between expected output and actual output:
-    diff .travis/test2.answer $TEMPDIR/test2.output
+    diff .travis/test2.answer "$TEMPDIR/test2.output"
     exit 1
 else
    echo test2 round3 passed
@@ -97,9 +97,9 @@ fi
 
 #test 2 test a non-existant file
 echo -e "/FAKEFILE\r\n" | src/gophernicus -h test.invalid -nf -nu -nv -nx -nf -nd -nr -r $TEMPDIR > $TEMPDIR/test2.output
-if ! cmp .travis/test2.answer $TEMPDIR/test2.output ; then
+if ! cmp .travis/test2.answer "$TEMPDIR/test2.output" ; then
     echo test2 round4 FAILED, test info saved at $TEMPDIR, diff between expected output and actual output:
-    diff .travis/test2.answer $TEMPDIR/test2.output
+    diff .travis/test2.answer "$TEMPDIR/test2.output"
     exit 1
 else
    echo test2 round4 passed
@@ -107,7 +107,7 @@ fi
 
 #test 3 test a non-existant GIF
 echo -e "/FAKE.GIF\n" | src/gophernicus -h test.invalid -nf -nu -nv -nx -nf -nd -nr -r $TEMPDIR > $TEMPDIR/test3.output
-if ! cmp .travis/test3.answer $TEMPDIR/test3.output ; then
+if ! cmp .travis/test3.answer "$TEMPDIR/test3.output" ; then
     'echo test3 round1 FAILED, test info saved at $TEMPDIR, diff not displayed (binary files expected)'
     exit 1
 else
@@ -116,7 +116,7 @@ fi
 
 #test 3 test a non-existant GIF
 echo -e "/FAKE.GIF\r\n" | src/gophernicus -h test.invalid -nf -nu -nv -nx -nf -nd -nr -r $TEMPDIR > $TEMPDIR/test3.output
-if ! cmp .travis/test3.answer $TEMPDIR/test3.output ; then
+if ! cmp .travis/test3.answer "$TEMPDIR/test3.output" ; then
     'echo test3 round2 FAILED, test info saved at $TEMPDIR, diff not displayed (binary files expected)'
     exit 1
 else
@@ -127,7 +127,7 @@ fi
 mkdir -p $TEMPDIR/extensions
 for ext in txt pl py sh tcl c cpp h log conf php php3 hqx Z gz tgz tar zip bz2 rar sea uue iso so o rtf ttf bin ics ical gif html htm xhtml css rdf rss xml svg jpg jpeg jfif png bmp ico xbm xpm pcx webp mbox eml mht mhtml doc ppt xls xlsx docx pptx mp3 mp2 wav mid midi wma flac ogg aiff aac m4a opus avi mp4 mpg mov qt asf mpv m4v webm ogv 3gp 3g2 wmv pic pict tif tiff pdf ps
 do
-  touch $TEMPDIR/extensions/empty.$ext
+  touch "$TEMPDIR/extensions/empty.$ext"
 done
 echo -e "extensions\n" | src/gophernicus -h test.invalid -nf -nu -nv -nx -nf -nd -nr -r $TEMPDIR > $TEMPDIR/test4.output
 if ! cmp .travis/test4.answer $TEMPDIR/test4.output ; then
@@ -188,50 +188,50 @@ echo -e '\000\000\001\263' > $TEMPDIR/magic/anothermpeg
 echo 'begin 644 fake.file' > $TEMPDIR/magic/uuencode
 echo -e 'begin-base64 644 fake.file\nCg==\n====' > $TEMPDIR/magic/uuencodeb64
 echo 'This is a plain text file I guess' > $TEMPDIR/magic/unknown
-echo -e "magic\n" | src/gophernicus -h test.invalid -nf -nu -nv -nx -nf -nd -nr -r $TEMPDIR > $TEMPDIR/test5.output
-if ! cmp .travis/test5.answer $TEMPDIR/test5.output ; then
+echo -e "magic\n" | src/gophernicus -h test.invalid -nf -nu -nv -nx -nf -nd -nr -r "$TEMPDIR" > $TEMPDIR/test5.output
+if ! cmp .travis/test5.answer "$TEMPDIR/test5.output" ; then
     echo test5 round1 FAILED, diff between expected and actual output
-    diff .travis/test5.answer $TEMPDIR/test5.output
+    diff .travis/test5.answer "$TEMPDIR/test5.output"
     exit 1
 else
    echo test5 round1 passed
 fi
-echo -e "magic\r\n" | src/gophernicus -h test.invalid -nf -nu -nv -nx -nf -nd -nr -r $TEMPDIR > $TEMPDIR/test5.output
-if ! cmp .travis/test5.answer $TEMPDIR/test5.output ; then
+echo -e "magic\r\n" | src/gophernicus -h test.invalid -nf -nu -nv -nx -nf -nd -nr -r "$TEMPDIR" > $TEMPDIR/test5.output
+if ! cmp .travis/test5.answer "$TEMPDIR/test5.output" ; then
     echo test5 round2 FAILED, diff between expected and actual output
-    diff .travis/test5.answer $TEMPDIR/test5.output
+    diff .travis/test5.answer "$TEMPDIR/test5.output"
     exit 1
 else
    echo test5 round2 passed
 fi
-echo -e "/magic\n" | src/gophernicus -h test.invalid -nf -nu -nv -nx -nf -nd -nr -r $TEMPDIR > $TEMPDIR/test5.output
-if ! cmp .travis/test5.answer $TEMPDIR/test5.output ; then
+echo -e "/magic\n" | src/gophernicus -h test.invalid -nf -nu -nv -nx -nf -nd -nr -r "$TEMPDIR" > $TEMPDIR/test5.output
+if ! cmp .travis/test5.answer "$TEMPDIR/test5.output" ; then
     echo test5 round3 FAILED, diff between expected and actual output
-    diff .travis/test5.answer $TEMPDIR/test5.output
+    diff .travis/test5.answer "$TEMPDIR/test5.output"
     exit 1
 else
    echo test5 round3 passed
 fi
-echo -e "/magic\r\n" | src/gophernicus -h test.invalid -nf -nu -nv -nx -nf -nd -nr -r $TEMPDIR > $TEMPDIR/test5.output
-if ! cmp .travis/test5.answer $TEMPDIR/test5.output ; then
+echo -e "/magic\r\n" | src/gophernicus -h test.invalid -nf -nu -nv -nx -nf -nd -nr -r "$TEMPDIR" > $TEMPDIR/test5.output
+if ! cmp .travis/test5.answer "$TEMPDIR/test5.output" ; then
     echo test5 round4 FAILED, diff between expected and actual output
-    diff .travis/test5.answer $TEMPDIR/test5.output
+    diff .travis/test5.answer "$TEMPDIR/test5.output"
     exit 1
 else
    echo test5 round4 passed
 fi
-echo -e "GET /magic\n" | src/gophernicus -h test.invalid -nf -nu -nv -nx -nf -nd -nr -r $TEMPDIR > $TEMPDIR/test5.output
-if ! cmp .travis/test5.answer $TEMPDIR/test5.output ; then
+echo -e "GET /magic\n" | src/gophernicus -h test.invalid -nf -nu -nv -nx -nf -nd -nr -r "$TEMPDIR" > $TEMPDIR/test5.output
+if ! cmp .travis/test5.answer "$TEMPDIR/test5.output" ; then
     echo test5 round5 FAILED, diff between expected and actual output
-    diff .travis/test5.answer $TEMPDIR/test5.output
+    diff .travis/test5.answer "$TEMPDIR/test5.output"
     exit 1
 else
    echo test5 round5 passed
 fi
-echo -e "GET /magic\r\n" | src/gophernicus -h test.invalid -nf -nu -nv -nx -nf -nd -nr -r $TEMPDIR > $TEMPDIR/test5.output
-if ! cmp .travis/test5.answer $TEMPDIR/test5.output ; then
+echo -e "GET /magic\r\n" | src/gophernicus -h test.invalid -nf -nu -nv -nx -nf -nd -nr -r "$TEMPDIR" > $TEMPDIR/test5.output
+if ! cmp .travis/test5.answer "$TEMPDIR/test5.output" ; then
     echo test5 round6 FAILED, diff between expected and actual output
-    diff .travis/test5.answer $TEMPDIR/test5.output
+    diff .travis/test5.answer "$TEMPDIR/test5.output"
     exit 1
 else
    echo test5 round6 passed
@@ -241,11 +241,11 @@ fi
 if [ "$(id -u)" -ne 0 ]; then
   echo 'test6 skipped (check if gophernicus rightly refuses to run as root)'
 else
-  echo -e "/\r\n" | src/gophernicus -h test.invalid -nf -nu -nv -nx -nf -nd -r $TEMPDIR > $TEMPDIR/test6.output
-if ! cmp .travis/test6.answer $TEMPDIR/test6.output ; then
-    echo test6 FAILED, diff between expected and actual output
+  echo -e "/\r\n" | src/gophernicus -h test.invalid -nf -nu -nv -nx -nf -nd -r "$TEMPDIR" > $TEMPDIR/test6.output
+if ! cmp .travis/test6.answer "$TEMPDIR/test6.output" ; then
+    echo "test6 FAILED, diff between expected and actual output"
     echo "DANGER DANGER DANGER, this gophernicus is UNSAFE to use in production!"
-    diff .travis/test6.answer $TEMPDIR/test6.output
+    diff .travis/test6.answer "$TEMPDIR/test6.output"
     exit 1
   else
     echo test6 passed
@@ -253,7 +253,7 @@ if ! cmp .travis/test6.answer $TEMPDIR/test6.output ; then
 fi
 
 echo "All important tests passed, deleting the temporary directory at $TEMPDIR"
-rm -rf $TEMPDIR
+rm -rf "$TEMPDIR"
 if ! src/gophernicus -v | grep -q 'Gophernicus/3.1.1 "zcrayfish fork"' ; then
     echo 'Possible version mismatch (if no errors ABOVE, this is not fatal)'
     exit 1
